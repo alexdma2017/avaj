fe5045901d25204766f7b74dbbf9a9d1bd8532d3
dma2 <adrianjarca@yahoo.com>
2017-08-02 13:41:05 +0300

    adaugare proiect

---
+++ Proiect1/src/Baloon.java
@@ -0,0 +1,33 @@
+
+public class Baloon extends Aircraft  implements Flyable {
+	
+	private WeatherTower weatherTower;
+
+	public Baloon( String name, Coordinates coordinates) {
+		super( name, coordinates);
+	
+	public void	updateConditions() {
+		
+	}
+	
+	public void registerTower( WeatherTower weatherTower) {
+		
+		
+	}
+		
+}
+
+	public void updateConditions() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void registerTower(WeatherTower weatherTower) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+
+}
